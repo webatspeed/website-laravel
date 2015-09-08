4725ffcd42aad906f42637c0eb3c6a80961bc3fe
Torsten Krohn <torsten.krohn@webatspeed.com>
2015-09-08 22:40:15 +0200

    Increase size of splash

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,69 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>@yield('title')</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="{{ url("img/favicon/ms-icon-144x144.png") }}">
-        <meta name="theme-color" content="#ffffff">
-        <link rel="apple-touch-icon" sizes="57x57" href="{{ url("img/favicon/apple-icon-57x57.png") }}">
-        <link rel="apple-touch-icon" sizes="60x60" href="{{ url("img/favicon/apple-icon-60x60.png") }}">
-        <link rel="apple-touch-icon" sizes="72x72" href="{{ url("img/favicon/apple-icon-72x72.png") }}">
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ url("img/favicon/apple-icon-76x76.png") }}">
-        <link rel="apple-touch-icon" sizes="114x114" href="{{ url("img/favicon/apple-icon-114x114.png") }}">
-        <link rel="apple-touch-icon" sizes="120x120" href="{{ url("img/favicon/apple-icon-120x120.png") }}">
-        <link rel="apple-touch-icon" sizes="144x144" href="{{ url("img/faviconapple-icon-144x144.png") }}">
-        <link rel="apple-touch-icon" sizes="152x152" href="{{ url("img/favicon/apple-icon-152x152.png") }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ url("img/favicon/apple-icon-180x180.png") }}">
-        <link rel="icon" type="image/png" sizes="192x192"  href="{{ url("img/favicon/android-icon-192x192.png") }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ url("img/favicon/favicon-32x32.png") }}">
-        <link rel="icon" type="image/png" sizes="96x96" href="{{ url("img/favicon/favicon-96x96.png") }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ url("img/favicon/favicon-16x16.png") }}">
-        <link rel="manifest" href="{{ url("img/favicon/manifest.json") }}">
-        <link rel="stylesheet" type="text/css" href="{{ elixir("css/app.css") }}">
-        <script src="{{ elixir("js/all.js") }}"></script>
-    </head>
-    <body>
-        <div class="wrap">
-            <nav class="navbar navbar-default navbar-fixed-top">
-                <div class="container">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Web at Speed</a>
-                    </div>
+<head>
+    <title>@yield('title')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ url("img/favicon/ms-icon-144x144.png") }}">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ url("img/favicon/apple-icon-57x57.png") }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ url("img/favicon/apple-icon-60x60.png") }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ url("img/favicon/apple-icon-72x72.png") }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ url("img/favicon/apple-icon-76x76.png") }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ url("img/favicon/apple-icon-114x114.png") }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ url("img/favicon/apple-icon-120x120.png") }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ url("img/faviconapple-icon-144x144.png") }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ url("img/favicon/apple-icon-152x152.png") }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url("img/favicon/apple-icon-180x180.png") }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ url("img/favicon/android-icon-192x192.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url("img/favicon/favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ url("img/favicon/favicon-96x96.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url("img/favicon/favicon-16x16.png") }}">
+    <link rel="manifest" href="{{ url("img/favicon/manifest.json") }}">
+    <link rel="stylesheet" type="text/css" href="{{ elixir("css/app.css") }}">
+    <script src="{{ elixir("js/all.js") }}"></script>
+</head>
+<body>
+<svg class="hidden" xmlns="http://www.w3.org/2000/svg">
+    <symbol id="shapeLongArrow" viewBox="288.1 406.4 19 29">
+        <g>
+            <polygon class="st0" points="297.6,433.9 289.1,425.4 290.6,424 297.6,431 304.6,424 306,425.4  "></polygon>
+            <rect x="296.6" y="407.4" class="st0" width="2" height="23.9"></rect>
+        </g>
+    </symbol>
+</svg>
+<div class="wrap">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Web at Speed</a>
+            </div>
 
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#">Dev <span class="sr-only">(current)</span></a></li>
-                            <li><a href="#">Start-Up</a></li>
-                            <li><a href="#">Devices</a></li>
-                            <li><a href="#">Usability</a></li>
-                            <li><a href="#">Customers</a></li>
-                            <li><a href="#">Headhunters</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                    </div><!-- /.navbar-collapse -->
-                </div><!-- /.container-fluid -->
-            </nav>
-            <div class="container-fluid text-center" id="splash">
-                {!! Html::image("img/logo_transparent.png") !!}<br/>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Dev <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Start-Up</a></li>
+                    <li><a href="#">Devices</a></li>
+                    <li><a href="#">Usability</a></li>
+                    <li><a href="#">Customers</a></li>
+                    <li><a href="#">Headhunters</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
             </div>
-            <div class="container-fluid">
-                @yield('content')
-            </div>
+            <!-- /.navbar-collapse -->
         </div>
-        <footer class="footer">
-            <div class="container text-center">
-                The current UNIX timestamp is {{ time() }}.
-            </div>
-        </footer>
-    </body>
+        <!-- /.container-fluid -->
+    </nav>
+    <div class="container-fluid text-center" id="splash">
+        {!! Html::image("img/logo_transparent.png") !!}<br/>
+    </div>
+    <div class="container-fluid">
+        @yield('content')
+    </div>
+</div>
+<footer class="footer">
+    <div class="container text-center">
+        The current UNIX timestamp is {{ time() }}.
+    </div>
+</footer>
+</body>
 </html>
