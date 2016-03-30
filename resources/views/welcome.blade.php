74568a19ba4bc73dd413a7c57423d3b24af6e18e
Torsten Krohn <torsten.krohn@webatspeed.com>
2016-03-30 08:44:54 +0200

    Fix headhunter section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -215,23 +215,18 @@
         <div class="container body-content">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h2>Headhunters</h2>
+                    <h2>Headhunters</h2>
 
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                    incididunt ut labore et
-                                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                    laboris nisi ut aliquip
-                                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                                    esse cillum dolore eu
-                                    fugiat nulla pariatur.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                        incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip
+                        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu
+                        fugiat nulla pariatur.
+                    </p>
                 </div>
+
             </div>
         </div>
     </div>
