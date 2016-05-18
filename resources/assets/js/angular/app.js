ac99be4d660bcd8ffe82640b849e80cb82760a19
Torsten Krohn <torsten.krohn@webatspeed.com>
2016-05-18 16:07:51 +0200

    Display current status

--- resources/assets/js/angular/app.js
+++ resources/assets/js/angular/app.js
@@ -1,16 +1,28 @@
+'use strict';
+
 (function () {
 
     var app = angular.module('app', []);
+
+    app.constant('status', {
+        UNSUBSCRIBED: 'unsubscribed',
+        PROCESSING: 'processing',
+        TO_CONFIRM_BY_USER: 'to-confirm-by-user',
+        TO_CONFIRM_BY_OWNER: 'to-confirm-by-owner',
+        SUBSCRIBED: 'subscribed',
+        ERROR: 'error'
+    });
 
     app.directive('cvNewsletter', function () {
             return {
                 restrict: 'E',
                 templateUrl: '/templates/cv-newsletter.html',
-                controller: function ($http) {
+                controller: function ($http, status) {
 
                     this.email = '';
-                    this.status = 'unsubscribed';
                     this.captchaResponse = '';
+                    this.status = status.UNSUBSCRIBED;
+                    this.STATUSES = status;
 
                     this.isFormValid = function (form) {
                         return form.$valid && this.captchaResponse != '';
@@ -24,6 +36,15 @@
                         return this.email != '' && !form.email.$valid;
                     };
 
+                    this.isStatus = function (status) {
+                        return this.status == status;
+                    };
+
+                    this.setStatus = function (status) {
+                        this.status = status;
+                        return true;
+                    };
+
                     this.subscribe = function () {
                         var _this = this;
                         var data = $.param({
@@ -32,9 +53,12 @@
                                 'captcha-response': _this.captchaResponse
                             })
                         });
-                        $http.post('https://www.webatspeed.eu/user/subscribe', data).success(function (data, status) {
-                            _this.status = 'unconfirmed';
-                        });
+                        $http.post('https://www.webatspeed.eu/user/subscribe', data)
+                            .then(function successCallback(response) {
+                                _this.setStatus(status.TO_CONFIRM_BY_USER);
+                            }, function errorCallback(response) {
+                                _this.setStatus(status.ERROR);
+                            });
                     };
                 },
                 controllerAs: 'cvn'
