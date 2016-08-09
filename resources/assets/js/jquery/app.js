bec774e411a7ba7733b38f7bb097b4628455ce5f
Torsten Krohn <torsten.krohn@webatspeed.com>
2016-08-10 00:16:21 +0200

    Remove manual scrolling

--- resources/assets/js/jquery/app.js
+++ resources/assets/js/jquery/app.js
@@ -45,14 +45,5 @@ $(function () {
         }
     });
 
-    // scroll to local anchor even if a query string is given
-    if (location.hash) {
-        var hash = location.hash;
-        if (hash.indexOf('?') > -1) {
-            hash = hash.split('?')[0];
-        }
-        $.smoothScroll({
-            scrollTarget: hash
-        });
-    }
+
 });
