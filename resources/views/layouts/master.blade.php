49247c877c7b68485466cb96564cdaf4202961cf
Torsten Krohn <torsten.krohn@webatspeed.com>
2016-11-15 09:02:35 +0100

    Set base of HTML5-mode application

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html lang="en" ng-app="app">
 <head>
     <title>@yield('title')</title>
+    <base href="/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="msapplication-TileColor" content="#ffffff">
